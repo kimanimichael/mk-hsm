164bbaa731fc93e830ffe753ff0c8737a8eef32b
mikael <muchunumike@gmail.com>
2025-05-26 07:36:39 +0300

    f(add_license): Add license info to all source files

--- ports/ESP32/include/ESP32_fsm.h
+++ ports/ESP32/include/ESP32_fsm.h
@@ -1,7 +1,21 @@
-/* ESP32 custom HSM header
- * Created by mikael on 3/18/25.
- */
+/**This program is a hierarchical state machines management framework.
+Copyright (C) 2025  Michael Kimani
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+In case of any enquiries, you can contact the author at muchunumike@gmail.com
+**/
 #ifndef ESP32HSM_H
 #define ESP32HSM_H
 
